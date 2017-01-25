3faaa0f70f5dc39854571b75bc08ee6106f8e196
Victor Guillen <victorguillen@Victors-MacBook-Air.local>
2017-01-25 11:41:34 -0800

    renders

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
+
 module.exports = {
+
   entry: "./lib/dots.js",
   output: {
-  	filename: "./lib/bundle.js"
+
+    filename: "./lib/bundle.js"
   },
-  devtool: 'source-map',
+
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
 };
